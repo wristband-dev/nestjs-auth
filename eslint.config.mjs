155e1594c7d48f00e21c77e27f178892b75eae98
Jim Verducci <jim@wristband.dev>
2024-09-22 15:08:01 -0700

    Refactor express-specific export

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,52 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
 
 export default [
   {
-    files: ["src/**/*.{ts}"],
-    ignores: ["src/**/*.spec.ts"],
+    files: ['src/**/*.{ts}'],
+    ignores: ['src/**/*.spec.ts'],
   },
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  {ignores: ["node_modules", "dist", "coverage", "test/**", "*.config.js"]},
+  prettier,
+  { ignores: ['node_modules', 'dist', 'coverage', 'test/**', '*.config.js'] },
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'arrow-body-style': ['error', 'always'],
+      'consistent-return': 'warn',
+      'func-names': 'off',
+      'no-console': 'off',
+      'no-empty-function': 'off',
+      'no-fallthrough': 'off',
+      'no-process-exit': 'off',
+      'no-unused-vars': 'warn',
+      'object-curly-newline': 'off',
+      'object-shorthand': 'off',
+      'prefer-destructuring': 'off',
+      'prettier/prettier': 'error',
+      strict: ['error', 'global'],
+      'max-len': [
+        'error',
+        {
+          code: 120,
+          ignoreComments: true,
+          ignoreRegExpLiterals: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+        },
+      ],
+      '@typescript-eslint/dot-notation': 'off',
+      'import/prefer-default-export': 'off',
+      'lines-between-class-members': 'off',
+      '@typescript-eslint/lines-between-class-members': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
 ];
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-export * from './auth.module';
-export * from './auth.service';
+import {
+  AuthConfig,
+  CallbackData,
+  CallbackResult,
+  CallbackResultType,
+  LoginConfig,
+  LogoutConfig,
+  TokenData,
+} from './types';
+
+export { WristbandExpressAuthModule } from './express/express-auth.module';
+export { WristbandExpressAuthService } from './express/express-auth.service';
+
+export type { AuthConfig, CallbackData, CallbackResult, LoginConfig, LogoutConfig, TokenData };
+export { CallbackResultType };
